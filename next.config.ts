3bd210baa0850af25a8377ae86ce4f640845e732
James Volpe <volpestyle@gmail.com>
2025-02-13 17:31:48 -0600

    add server side env variables to next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    // Server-side only variables
+    ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
+    SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
+    REGION: process.env.REGION,
+    AMADEUS_API_KEY: process.env.AMADEUS_API_KEY,
+    AMADEUS_API_SECRET: process.env.AMADEUS_API_SECRET,
+    AMADEUS_API_URL: process.env.AMADEUS_API_URL,
+  },
 };
 
 export default nextConfig;
